78aba538f17ad20f847e100f8e90f3a2851949a7
shumorkiniv <shumorkiniv@intersvyaz.net>
2021-03-12 21:38:28 +0500

    Laguage swith
    - Add one more check
    - Change russian comments to english
    - Made code more readable
    - Add php 7.1+ features

--- src/InnValidator.php
+++ src/InnValidator.php
@@ -6,65 +6,84 @@ use yii\validators\Validator;
 
 /**
  * Class InnValidator
- * Валидатор ИНН
+ * InnValidator validates that the attribute value is valid INN.
+ *
+ * Note, this validator should only be used with string-typed or integer-typed attributes.
+ *
+ * @author Shumorkin Ilya <shumorkinilya@mail.ru>
  */
 class InnValidator extends Validator
 {
-    /** @var int  */
-    const DIVISIOR = 11;
-    /** @var int Индекс числа с которым сравнивается контрольное */
-    const COMPARED_NUMBER_TEN_INDEX = 9;
-    /** @var int Индекс числа с которым сравнивается первое контрольное */
-    const COMPARED_NUMBER_TWELVE_INDEX1 = 10;
-    /** @var int Индекс числа с которым сравнивается второе контрольное */
-    const COMPARED_NUMBER_TWELVE_INDEX2 = 11;
-    /** @var string Сообщение о неверном количестве знаков */
-    const WRONG_COUNT_ERROR_MESSAGE = 'ИНН должен состоять из 11 или 12 чисел.';
-    /** @var string Сообщение о неверном символе */
-    const WRONG_CHAR_ERROR_MESSAGE = 'ИНН должен состоять только из цифр.';
-    /** @var string Сообщение при проваленной проверки контрольных чисел */
-    const INVALID_VALUE_ERROR_MESSAGE = 'Несуществующий ИНН.';
+    /** @var int Divider for check sum */
+    const DIVIDER = 11;
+    /** @var int Index of number wich compare with control */
+    const COMPARED_NUMBER_LEGAL_INDEX = 9;
+    /** @var int Index of number wich compare with first control */
+    const COMPARED_NUMBER_INDIVIDUAL_INDEX1 = 10;
+    /** @var int Index of number wich compare with second control */
+    const COMPARED_NUMBER_INDIVIDUAL_INDEX2 = 11;
+    /** @var int Length of INN for legal entities */
+    const LEGAL_ENTITY_LENGTH = 10;
+    /** @var int Length of INN for individuals */
+    const INDIVIDUAL_LENGTH = 12;
 
-    /** @var int[] Коэффициенты для проверки первой контрольной цифры 12-значного ИНН */
-    private $twelveFirstCoefficients = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8];
-    /** @var int[] Коэффициенты для проверки второй контрольной цифры 12-значного ИНН */
-    private $twelveSecondCoefficients = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8];
-    /** @var int[] Коэффициенты для проверки первой контрольной цифры 10-значного ИНН */
-    private $tenCoefficients = [2, 4, 10, 3, 5, 9, 4, 6, 8];
+    /** @var string[] Default error messages */
+    private const DEFAULT_ERROR_MESSAGES = [
+        'wrongLength' => 'ИНН должен состоять из 11 или 12 чисел.',
+        'wrongChar' => 'ИНН должен состоять только из цифр.',
+        'invalidValue' => 'Несуществующий ИНН.',
+    ];
+
+    /** @var int[] Coefficients for check first check number of 12-digit INN */
+    private const N1_COEFFICIENTS_INDIVIDUAL = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8];
+    /** @var int[] Coefficients for check first check number of 12-digit INN */
+    private const N2_COEFFICIENTS_INDIVIDUAL = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8];
+    /** @var int[] Coefficients for check check number of 10-digit INN */
+    private const N1_COEFFICIENTS_LEGAL_ENTITY = [2, 4, 10, 3, 5, 9, 4, 6, 8];
+
+    /** @var string User wrong count message */
+    public string $wrongLengthMessage;
+    /** @var string User wrong character message */
+    public string $wrongCharMessage;
+
     /** @var array */
-    private $numbers;
+    private array $numbers;
     /** @var int */
-    private $numbersCount;
+    private int $length;
 
     /**
      * @inheritdoc
      */
-    protected function validateValue($value)
+    protected function validateValue($value): ?array
     {
+        if (!is_int($value) || !is_string($value)) {
+            return [$this->message ?: self::DEFAULT_ERROR_MESSAGES['invalidValue'], []];
+        }
+
         if (is_int($value)) {
             $value = (string)$value;
         }
 
-        if (strlen($value) !== 10 && strlen($value) !== 12) {
-            return [self::WRONG_COUNT_ERROR_MESSAGE, ''];
+        if (strlen($value) !== self::LEGAL_ENTITY_LENGTH && strlen($value) !== self::INDIVIDUAL_LENGTH) {
+            return [$this->wrongLengthMessage ?: self::DEFAULT_ERROR_MESSAGES['wrongLength'], []];
         }
 
         if (!is_numeric($value)) {
-            return [self::WRONG_CHAR_ERROR_MESSAGE, ''];
+            return [$this->wrongCharMessage ?: self::DEFAULT_ERROR_MESSAGES['wrongChar'], []];
         }
 
         $this->numbers = array_map('intval', str_split($value));
-        $this->numbersCount = count($this->numbers);
+        $this->length = count($this->numbers);
 
-        if ($this->numbersCount === 10) {
-            if (!$this->checkFirstCheckNumber()) {
-                return [self::INVALID_VALUE_ERROR_MESSAGE, ''];
+        if ($this->length === self::LEGAL_ENTITY_LENGTH) {
+            if (!$this->checkN1CheckNumber()) {
+                return [$this->message ?: self::DEFAULT_ERROR_MESSAGES['invalidValue'], []];
             }
         }
 
-        if ($this->numbersCount === 12) {
-            if (!$this->checkFirstCheckNumber() || !$this->checkSecondCheckNumber()) {
-                return [self::INVALID_VALUE_ERROR_MESSAGE, ''];
+        if ($this->length === self::INDIVIDUAL_LENGTH) {
+            if (!$this->checkN1CheckNumber() || !$this->checkN2CheckNumber()) {
+                return [$this->message ?: self::DEFAULT_ERROR_MESSAGES['invalidValue'], []];
             }
         }
 
@@ -72,41 +91,43 @@ class InnValidator extends Validator
     }
 
     /**
-     * Проверка первого контрольного числа
+     * Check of n1 - first check number
      *
      * @return bool
      */
-    private function checkFirstCheckNumber()
+    private function checkN1CheckNumber(): bool
     {
-        $coefficients = $this->numbersCount == 10 ? $this->tenCoefficients : $this->twelveFirstCoefficients;
+        $isLegal = $this->length === self::LEGAL_ENTITY_LENGTH;
+
+        $coefficients = $isLegal ? self::N1_COEFFICIENTS_LEGAL_ENTITY : self::N1_COEFFICIENTS_INDIVIDUAL;
         $checkNumber = $this->calculateCheckNumber($this->calculateCheckSum($coefficients));
 
-        if ($this->numbersCount === 10) {
-            return $checkNumber === $this->numbers[self::COMPARED_NUMBER_TEN_INDEX];
+        if ($isLegal) {
+            return $checkNumber === $this->numbers[self::COMPARED_NUMBER_LEGAL_INDEX];
         }
 
-        return $checkNumber === $this->numbers[self::COMPARED_NUMBER_TWELVE_INDEX1];
+        return $checkNumber === $this->numbers[self::COMPARED_NUMBER_INDIVIDUAL_INDEX1];
     }
 
     /**
-     * Проверка второго контрольного числа (только для 12-значных)
+     * Check of n2 - seond check number (only for individual)
      *
      * @return bool
      */
-    private function checkSecondCheckNumber()
+    private function checkN2CheckNumber(): bool
     {
         $checkNumber = $this->calculateCheckNumber($this->calculateCheckSum($this->twelveSecondCoefficients));
 
-        return $checkNumber === $this->numbers[self::COMPARED_NUMBER_TWELVE_INDEX2];
+        return $checkNumber === $this->numbers[self::COMPARED_NUMBER_INDIVIDUAL_INDEX2];
     }
 
     /**
-     * Вычисление контрольной суммы
+     * Calculating of check sum
      *
-     * @param $coefficients
-     * @return float|int
+     * @param int[] $coefficients
+     * @return int
      */
-    private function calculateCheckSum($coefficients)
+    private function calculateCheckSum(array $coefficients): int
     {
         $checkSum = 0;
 
@@ -120,16 +141,16 @@ class InnValidator extends Validator
     }
 
     /**
-     * Вычисление контрольного числа
+     * Calculating of check number
      *
-     * @param $checkSum
-     * @return float|int
+     * @param int $checkSum
+     * @return int
      */
-    private function calculateCheckNumber($checkSum)
+    private function calculateCheckNumber(int $checkSum): int
     {
-        $diff = $checkSum / self::DIVISIOR;
+        $diff = $checkSum / self::DIVIDER;
 
-        $checkNumber = $checkSum - self::DIVISIOR * (int)$diff;
+        $checkNumber = $checkSum - self::DIVIDER * (int)$diff;
 
         if ($checkNumber > 9) {
             $checkNumber = $checkNumber % 10;
